f47f4820ea8a9de9425805f2ae40bfc046e27940
dmandal2 <dpkmandal1992@gmail.com>
2025-07-19 18:28:49 +0530

    Added RetryAnalyzer and FlakyTest for Phase 2.2 basic retry logic

--- src/main/java/com/retrythesis/retry/RetryAnalyzer.java
+++ src/main/java/com/retrythesis/retry/RetryAnalyzer.java
@@ -1,5 +1,19 @@
 package com.retrythesis.retry;
 
-public class RetryAnalyzer {
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
 
+public class RetryAnalyzer implements IRetryAnalyzer {
+	private int retryCount = 0;
+	private static final int maxRetryCount = 2; // Retry 2 times
+
+	@Override
+	public boolean retry(ITestResult result) {
+		if (retryCount < maxRetryCount) {
+			System.out.println("Retrying test: " + result.getName() + " | Attempt: " + (retryCount + 1));
+			retryCount++;
+			return true;
+		}
+		return false;
+	}
 }
