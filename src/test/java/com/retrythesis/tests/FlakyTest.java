d8f12c432d87aa03ab330dc0dd975413cda12f94
dmandal2 <dpkmandal1992@gmail.com>
2025-08-08 22:00:53 +0530

    Modified test file with logger

--- src/test/java/com/retrythesis/tests/FlakyTest.java
+++ src/test/java/com/retrythesis/tests/FlakyTest.java
@@ -2,13 +2,20 @@ package com.retrythesis.tests;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.apache.log4j.Logger;  // Import log4j
 
 public class FlakyTest {
 
-	@Test(retryAnalyzer = RetryAnalyzer.class)
-	public void testRandomFlaky() {
-		double randomValue = Math.random(); // generates value between 0.0 and 1.0
-		System.out.println("Generated value: " + randomValue);
-		Assert.assertTrue(randomValue > 0.5, "Flaky test failed: value <= 0.5");
-	}
+    // Create a logger instance
+    static Logger logger = Logger.getLogger(FlakyTest.class);
+
+    @Test(retryAnalyzer = RetryAnalyzer.class)
+    public void testRandomFlaky() {
+        double randomValue = Math.random(); // generates value between 0.0 and 1.0
+
+        // Log instead of just printing
+        logger.info("Generated value: " + randomValue);
+
+        Assert.assertTrue(randomValue > 0.5, "Flaky test failed: value <= 0.5");
+    }
 }
