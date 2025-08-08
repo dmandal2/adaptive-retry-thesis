42be357dd81443d3030e224e17b38f11ff087805
dmandal2 <dpkmandal1992@gmail.com>
2025-08-08 19:59:22 +0530

    Added SLF4J logging to RetryAnalyzer

--- src/test/java/com/retrythesis/tests/RetryAnalyzer.java
+++ src/test/java/com/retrythesis/tests/RetryAnalyzer.java
@@ -2,18 +2,24 @@ package com.retrythesis.tests;
 
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RetryAnalyzer implements IRetryAnalyzer {
-	private int retryCount = 0;
-	private static final int maxRetryCount = 2; // Retry 2 times
+    private int retryCount = 0;
+    private static final int maxRetryCount = 2;
+    private static final Logger logger = LoggerFactory.getLogger(RetryAnalyzer.class);
 
-	@Override
-	public boolean retry(ITestResult result) {
-		if (retryCount < maxRetryCount) {
-			System.out.println("Retrying test: " + result.getName() + " | Attempt: " + (retryCount + 1));
-			retryCount++;
-			return true;
-		}
-		return false;
-	}
+    @Override
+    public boolean retry(ITestResult result) {
+        if (retryCount < maxRetryCount) {
+            logger.info("Retrying test '{}' | Attempt: {} | Time: {}", 
+                        result.getName(), retryCount + 1, System.currentTimeMillis());
+            retryCount++;
+            return true;
+        } else {
+            logger.info("Test '{}' failed after {} attempts.", result.getName(), retryCount);
+        }
+        return false;
+    }
 }
